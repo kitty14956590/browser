2bac17949613cf153853c0de54d876fba55a5db7
kitty14956590 <borbtrash@gmail.com>
2024-04-16 19:46:46 -0400

    meow

--- includes/linked.h
+++ includes/linked.h
@@ -2,3 +2,9 @@ typedef struct linked {
 	void * ptr;
 	struct linked * next;
 } linked_t;
+
+typedef int (* linked_callback_t)(linked_t *);
+
+linked_t * alloc_linked();
+linked_t * alloc_list(int length);
+int iterate_list(linked_callback_t callback, linked_t * list);
--- src/linked.c
+++ src/linked.c
@@ -13,10 +13,27 @@ linked_t * alloc_linked() {
 
 linked_t * alloc_list(int length) {
 	linked_t * root = alloc_linked();
+	linked_t * top = root;
+	root->ptr = (linked_t *) 0;
+	root->next = (linked_t *) 0;
 	if (!length) {
-		root->ptr = (linked_t *) 0;
-		root->next = (linked_t *) 0;
 		return root; // yayy !! here you go !!
 	}
-	
+	while (length--) {
+		top->next = alloc_linked();
+		top = top->next;
+	}
+	top->next = (linked_t *) 0;
+	return root;
+}
+
+int iterate_list(linked_callback_t callback, linked_t * list) {
+	while (list) {
+		int retval = callback(list);
+		if (retval) {
+			return retval;
+		}
+		list = list->next;
+	}
+	return 0;
 }
